2c37325cc9f13de60ca93a621b1d79fc6c91f624
Kargin Nikita <hei@spark-mail.ru>
2016-10-31 01:45:38 +0300

    Added annos

--- src/main/java/wiring/Guitar.java
+++ src/main/java/wiring/Guitar.java
@@ -1,8 +1,10 @@
 package wiring;
 
 import annotations.StringedInstrument;
+import org.springframework.stereotype.Component;
 
 @StringedInstrument
+@Component
 public class Guitar implements Instrument {
     public Guitar() {
     }
