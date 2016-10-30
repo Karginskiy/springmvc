c738603494bf82f88fcb56b290d37694d1e85c3e
Kargin Nikita <hei@spark-mail.ru>
2016-10-30 18:03:48 +0300

    BraveKnight with Minstrel-pojo

--- src/main/java/BraveKnight.java
+++ src/main/java/BraveKnight.java
@@ -1,13 +1,16 @@
 public class BraveKnight implements Knight {
 
     private Quest quest;
+    private Minstrel minstrel;
 
     public BraveKnight(Quest quest) {
         this.quest = quest;
     }
 
     public void embarkOnQuest() throws QuestException {
+        minstrel.singBeforeQuest();
         quest.embark();
+        minstrel.singAfterQuest();
     }
 
 }
