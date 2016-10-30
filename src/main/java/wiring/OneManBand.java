e38dc869c579a4a780b987a07fe9ede6a74cd480
Kargin Nikita <hei@spark-mail.ru>
2016-10-30 20:56:20 +0300

    Added bean with collection inject

---
+++ src/main/java/wiring/OneManBand.java
@@ -0,0 +1,19 @@
+package wiring;
+
+import java.util.Collection;
+
+public class OneManBand implements Performer {
+
+    private Collection<Instrument> instruments;
+    
+    @Override
+    public void perform() throws PerformanceException {
+        for (Instrument instrument : instruments) {
+            instrument.play();
+        }
+    }
+
+    public void setInstruments(Collection<Instrument> instruments) {
+        this.instruments = instruments;
+    }
+}
